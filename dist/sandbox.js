7e64b0e164b1b99adf8fe106f2c3776a1ad699b8
swimmingcircle119 <lampmaa22@gmail.com>
2024-05-09 10:06:59 -0700

    done test

---
+++ dist/sandbox.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("test");
+const users = [1, 2, 4, 5, 6];
+users.forEach((user) => console.log(user));
+console.log("test1");
---
+++ src/sandbox.ts
@@ -0,0 +1,5 @@
+console.log("test");
+
+const users: number[] = [1, 2, 4, 5, 6];
+users.forEach((user) => console.log(user));
+console.log("test1");
